648db004651b1fed210e33d95818e4fb6c15d67c
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-07-21 23:37:09 +0500

    Building the language selector

--- tests/Feature/LanguageGlobalsTest.php
+++ tests/Feature/LanguageGlobalsTest.php
@@ -2,6 +2,14 @@
 
 use Inertia\Testing\AssertableInertia;
 
+it('containt the current selected language', function () {
+    app()->setLocale('ru');
+    $this->get('/')
+        ->assertInertia(function (AssertableInertia $page) {
+            $page->where('language', 'ru');
+        });
+});
+
 it('contains a list of availiable languages', function () {
     $this->get('/')
         ->assertInertia(function (AssertableInertia $page) {
