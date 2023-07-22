50dc5d0fb538ea7f80a83e0dc53727f13dcdd38f
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-07-22 11:39:20 +0500

    Sharing all translations with Inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use App\Http\Resources\LanguageResource;
 use App\Lang\Lang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
 
@@ -33,16 +35,32 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+
             'auth' => [
                 'user' => $request->user(),
             ],
+
             'language' => app()->getLocale(),
+
             'languages' => LanguageResource::collection(Lang::cases()),
+
+            'translations' => function () {
+
+                return collect(File::allFiles(base_path('lang/'.app()->getLocale())))
+                    ->flatMap(function ($file) {
+                        return Arr::dot(
+                            File::getRequire($file->getRealPath()),
+                            $file->getBasename('.'.$file->getExtension()).'.',
+                        );
+                    });
+            },
+
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
                     'location' => $request->url(),
                 ]);
             },
+
         ]);
     }
 }
--- tests/Feature/LanguageGlobalsTest.php
+++ tests/Feature/LanguageGlobalsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Inertia\Testing\AssertableInertia;
 
 it('containt the current selected language', function () {
@@ -15,5 +16,15 @@ it('contains a list of availiable languages', function () {
         ->assertInertia(function (AssertableInertia $page) {
             $page->where('languages.0.value', 'en')
                 ->where('languages.0.label', 'English');
+        });
+});
+
+it('contains all translations', function () {
+    app()->setLocale('en');
+    $this->get('/')
+        ->assertInertia(function (AssertableInertia $page) {
+            expect(Arr::get($page->toArray(), 'props.translations'))->toMatchArray([
+                'auth.failed' => 'These credentials do not match our records.',
+            ]);
         });
 });
